ab23f01a44003dc4196a984e5a103bf97256eb77
Vert <alex.c.goble@gmail.com>
2023-11-02 14:56:46 -0400

    include long description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="perftracker",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/vertyco/perftracker",
-    author="Your Name",
+    author="Alex Goble",
     author_email="alex.c.goble@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description="A Python performance tracking package",
     packages=find_packages(),
     install_requires=["pydantic"],
