83ab0c67c181bfafaa2159c8976971c6f61112b7
Vert <alex.c.goble@gmail.com>
2023-11-03 23:09:24 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="perftracker",
-    version="1.0.4",
+    version="1.0.5",
     url="https://github.com/vertyco/perftracker",
     author="vertyco",
     author_email="alex.c.goble@gmail.com",
